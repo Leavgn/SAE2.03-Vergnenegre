5219446899be10299e2a3617ac0df24469e00839
Vergnenegre Lea <vergnenegre8@mmi-dev.unilim.fr>
2026-04-22 15:11:27 +0200

    Test Clé SSh

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "vergnenegre8");
+define("DBLOGIN", "vergnenegre8");
+define("DBPWD", "vergnenegre8");
 
 
 function getAllMovies(){
