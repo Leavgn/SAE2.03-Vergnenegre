517a40e451b28b5702887878ad675a35661dcefc
Vergnenegre Lea <vergnenegre8@mmi-dev.unilim.fr>
2026-05-06 17:00:14 +0200

    modifications

--- IterationSurprise/app/component/MovieCategory/script.js
+++ IterationSurprise/app/component/MovieCategory/script.js
@@ -5,18 +5,14 @@ let template = await templateFile.text();
 
 let MovieCategory = {};
 
-/* Retourne le HTML d'une catégorie avec son nom et la liste de ses films. */
 MovieCategory.format = function (categoryName, movies) {
-  let html = template;
   let moviesHtml = "";
-  // Génère le HTML de chaque film en réutilisant le composant MovieList
   for (const movie of movies) {
     moviesHtml += MovieList.format(movie);
   }
-  html = html.replace("{{categoryName}}", categoryName); // Présent deux fois dans le template
-  html = html.replace("{{categoryName}}", categoryName);
+  let html = template;
+  html = html.replaceAll("{{categoryName}}", categoryName);
   html = html.replace("{{movies}}", moviesHtml);
-
   return html;
 };
 
